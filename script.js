9a0dd8b0e5db66f6db52baf43aede15434128f61
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-08-12 15:43:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 //your code here
-
+let main = document.getElementById("app");
+let swap = document.getElementById("swap");
+swap.addEventListener("click" , function(){
+	main.style.backgroundColor="black";
+})
